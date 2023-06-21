f3722b89d8801c657d7b096349ec03ef6b636374
Janvierjavi <janvierjavim2@gmail.com>
2023-06-21 09:24:42 +0200

    install tailwind and config it into app

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <>
       <main>
-        <h1>my name is janvier Mutuyimana</h1>
+        <h1 className="text-red-900">my name is janvier Mutuyimana</h1>
       </main>
     </>
   );
