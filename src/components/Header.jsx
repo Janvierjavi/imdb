82f3a97bcbbd59831ee4fc28d1a46c529ce25e48
Janvierjavi <janvierjavim2@gmail.com>
2023-06-24 07:18:39 +0200

    fix text color and select non act only on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ import DarkModeSwitch from "./DarkModeSwitch";
 
 export default function Header() {
   return (
-    <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center">
+    <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center select-none">
       <div className="flex">
         <MenuItem title="HOME" address="/" Icon={AiFillHome} />
         <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill} />
