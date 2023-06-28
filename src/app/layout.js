6f6edc7842231be0379e3390b1796b5bc8c94606
Janvierjavi <janvierjavim2@gmail.com>
2023-06-28 13:10:01 +0200

    add search ability

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import "./globals.css";
 import Providers from "./Providers";
 import Navbar from "@/components/Navbar";
+import SearchBox from "@/components/SearchBox";
 // import { Inter } from "next/font/google";
 // const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
           {/* Navbar*/}
           <Navbar />
           {/* SearchBox*/}
+          <SearchBox />
           {children}
         </Providers>
       </body>
